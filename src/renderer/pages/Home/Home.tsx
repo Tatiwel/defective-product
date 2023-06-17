8c89155f0463f3ae0bd3117e8d78830f777a949b
Tatiwel <49209072+Tatiwel@users.noreply.github.com>
2023-06-17 16:16:43 -0300

    Pequenas atualizçoes
    Atualização no css

--- src/renderer/pages/Home/Home.tsx
+++ src/renderer/pages/Home/Home.tsx
@@ -1,32 +1,14 @@
-import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
 import NavBar from "renderer/components/NavBar";
-
 import logo_image from "../../public/images/Logo.png";
 import mascote_image from "../../public/images/Mascote_Indicando.png";
-
-// const windowProduct = async () => {
-//   const remote = require('electron').remote;
-//   const BrowserWindow = remote.BrowserWindow;
-//   const win = new BrowserWindow({
-//     height: 600,
-//     width: 800,
-//     frame: false,
-//     webPreferences: {
-//       nodeIntegration: true,
-//     },
-//   });
-//   win.loadURL(`file://${__dirname}/app.html#/product`);
-// };
 
 function Home() {
   return (
     <div className="header">
       <Row>
         <NavBar title="Home" />
-        <Col sm>
+        <Col md="auto">
           <div className="background-image">
             <h1>This is a Home Page</h1>
             <img className="logo-image" src={logo_image} alt="logo-image" />
